5642f3bdcb0fa4b175f3eb1c2f1b25b77aff4b3c
jonam <jadhikari131@gmail.com>
2021-10-31 16:52:10 +0900

    adding bootstrap

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path('',views.index)
+    path('',views.chino),
+    path('index/',views.index),
+    
 }
--- index/views.py
+++ index/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index/index.html')
+
+def chino(request):
+    return render(request, 'index.html')
