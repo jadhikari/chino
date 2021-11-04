b039a56c761c1c6658add430882faa99214d3b8d
jonam <jadhikari131@gmail.com>
2021-11-04 23:09:05 +0900

    adding slide image

--- index/urls.py
+++ index/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = {
     path('',views.chino),
-    path('index/',views.index),
-    
+    path('about',views.about),
 }
--- index/views.py
+++ index/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http.response import HttpResponse, JsonResponse
 # Create your views here.
 
 
-def index(request):
-    return render(request,'index/index.html')
 
 def chino(request):
-    return render(request, 'index.html')
+    return render(request, 'main.html')
+
+def about(request):
+    return render(request,'about.html')
